b9b707d51d476ce711ab34bf06df701e83c831e0
Isiah Arrufat <isiaharrufat@pursuit.org>
2023-10-18 03:27:02 -0400

    updated my flowerLibrary.json with smaple data from mockaroo and well have all fuctions working in my CRUD app

--- src/flowersController.js
+++ src/flowersController.js
@@ -8,7 +8,7 @@ const flowerLibrary = require('../data/flowerLibrary.json');
 
 const create = (flowersList, flowerName) => {
     
-    const existingFlower = flowerLibrary.find((flower) => flower.name === flowerName);
+    const existingFlower = flowerLibrary.find((flower) => flower.flower_name === flowerName);
     
 console.log(existingFlower);
 
@@ -17,9 +17,9 @@ console.log(existingFlower);
       flowersList.push(existingFlower)
     }else {
         const newFlower = {
-            name: flowerName,
+            flower_name: flowerName,
             id: nanoid(4),
-            shelfLife: 7
+            petal_count: 80
           };
         
           flowersList.push(newFlower);
@@ -34,12 +34,12 @@ console.log(existingFlower);
 
 
 const index = (flowersList) => {
-    return flowersList.map((flower) => flower.id + ' ' + flower.name).join('\n');
+    return flowersList.map((flower) => flower.id + ' ' + flower.flower_name).join('\n');
   }
 
 const show = (flowersList, flowersId) => {
     const flower = flowersList.find((flower) => flower.id === flowersId);
-    return flower.id + ' ' + flower.name + ' ' + flower.priceInCents + ' ' + flower.inStock + ' ' + flower.color + ' ' + flower.origin + ' ' + flower.shelfLife;
+    return `flowerid:${flower.id} flowerName:${flower.flower_name} petal_count:${flower.petal_count} stemLength:${flower.stem_length} flowerColor:${flower.color} wateringNeeds:${flower.watering_needs} growingSeason:${flower.growing_season}`;
   }
 
 const inform = console.log;
@@ -60,8 +60,8 @@ const edit = (flowersList, flowerId, updatedFlower) => {
     const index = flowersList.findIndex((flower) => flower.id === flowerId);
     if (index > -1) {
       flowersList[index].id = flowerId;
-      flowersList[index].name = updatedFlower;
-      flowersList[index].shelfLife = flowersList[updatedFlower] || 7;
+      flowersList[index].flower_name = updatedFlower;
+      flowersList[index].petal_count = flowersList[updatedFlower] || 70;
       inform('flower successfully updated');
       return flowersList;
     } else {
@@ -70,8 +70,8 @@ const edit = (flowersList, flowerId, updatedFlower) => {
     }
   }
 
- const score = (animals) => {
-    return flowers.reduce((acc, current) => acc + current.priceInCents, 0);
+ const score = (flowersList) => {
+    return flowersList.reduce((acc, current) => acc + current.petal_count, 0);
   }
   
   
